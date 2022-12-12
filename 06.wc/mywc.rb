f05d1984dbdba05d6014beb8d1a151b204ffb149
nicole2525 <nicole2525nomail@gmail.com>
2022-12-12 22:19:37 +0900

    2度目のレビュー箇所を修正

--- 06.wc/mywc.rb
+++ 06.wc/mywc.rb
@@ -4,50 +4,63 @@
 require 'optparse'
 
 def main
-  options = decision_option
+  option = decide_option
   argv_size = ARGV.size
-  count_result = execution(ARGV.size.zero? ? standard_input : filename, options)
-  display_results(count_result, argv_size)
+  count_target = format_count_target(argv_size)
+  display_result(count_target, option)
+  display_total(count_target, option) if argv_size > 1
 end
 
-def decision_option
+def decide_option
   option = ARGV.getopts('l', 'w', 'c')
   no_option = { 'l' => true, 'w' => true, 'c' => true }
   option.value?(true) ? option : no_option
 end
 
-def standard_input
-  $stdin.readlines
-end
-
-def filename
-  ARGV.map { |file_name| File.open(file_name).read.to_s }
-end
-
-def execution(argument, options)
-  results = []
-  n = 0
-  argument.each do |file|
-    results << file.split("\n").size if options['l']
-    results << file.split(nil).size if options['w']
-    results << file.bytesize if options['c']
-    results << " #{ARGV[n]}\n"
-    n += 1
-  end
-  results
-end
-
-def display_results(count_result, argv_size)
-  divisor = argv_size.zero? ? count_result.count(" \n") : argv_size
-  total_preparation = count_result.each_slice(count_result.size / divisor).to_a.transpose
-  total_preparation.pop
-  total_results = total_preparation.map { |t| t.map(&:to_i).sum.to_s.rjust(8) }.join('')
-  if argv_size >= 1
-    print count_result.map { |r| r.to_s.rjust(8) }.join('')
-    print "#{total_results} total"
+def format_count_target(argv_size)
+  if argv_size.zero?
+    content = $stdin.read
+    { '' => content }
   else
-    print total_results
+    contents = ARGV.map { |file_name| File.open(file_name).read }
+    ARGV.zip(contents).to_h
   end
+end
+
+def count_line(target_content)
+  target_content.split("\n").size
+end
+
+def count_word(target_content)
+  target_content.split(nil).size
+end
+
+def count_byte(target_content)
+  target_content.bytesize
+end
+
+def display_result(count_target, option)
+  count_target.each do |file_name, target_content|
+    print count_line(target_content).to_s.rjust(8) if option['l']
+    print count_word(target_content).to_s.rjust(8) if option['w']
+    print count_byte(target_content).to_s.rjust(8) if option['c']
+    print " #{file_name}\n"
+  end
+end
+
+def display_total(count_target, option)
+  line_number = 0
+  word_number = 0
+  byte_number = 0
+  count_target.each_value do |target_content|
+    line_number += count_line(target_content)
+    word_number += count_word(target_content)
+    byte_number += count_byte(target_content)
+  end
+  line_number_display = line_number.to_s.rjust(8) if option['l']
+  word_number_display = word_number.to_s.rjust(8) if option['w']
+  byte_number_display = byte_number.to_s.rjust(8) if option['c']
+  print "#{line_number_display}#{word_number_display}#{byte_number_display} total"
 end
 
 main
