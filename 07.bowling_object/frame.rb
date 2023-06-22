2034bde325c074d3a743d8fa83baa405c066a093
nicole2525 <nicole2525nomail@gmail.com>
2023-06-22 20:06:11 +0900

    rubocopの指摘事項によりcalc_scoresメソッドの記述を修正

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -10,7 +10,7 @@ class Frame
   end
 
   def calc_scores(next_frame, after_next_frame = nil)
-    if !next_frame = nil && calc_regular_scores == 10
+    if !next_frame.nil? && calc_regular_scores == 10
       10 +
         if double?(next_frame)
           next_point(next_frame) + (after_next_frame.nil? ? next_frame.second_shot.score : after_next_frame.first_shot.score)
