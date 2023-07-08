f4db2e68f6fcf8ea270845cfb53c48ea792ab2ec
nicole2525 <nicole2525nomail@gmail.com>
2023-07-08 17:30:57 +0900

    ls.rbにメイン処理を集約

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -2,9 +2,22 @@
 
 require 'optparse'
 require_relative './file_list'
-require_relative './ls_execution_result'
-require_relative './file_details'
+require_relative './file_display'
+require_relative './file_detail'
 
-options = ARGV.getopts('a', 'r', 'l')
-ls_execution_result = LsExecutionResult.new(options)
-ls_execution_result.display(options)
+OPTIONS = ARGV.getopts('a', 'r', 'l')
+file_list = FileList.new(OPTIONS)
+file_names = file_list.file_names_list
+
+return if file_names.empty?
+
+if OPTIONS['l']
+  file_details = file_list.file_details_list
+  details_and_options = { displayed_file_information: file_details, options: OPTIONS }
+  displayed_details_list = FileDisplay.new(details_and_options)
+  displayed_details_list.display_details
+else
+  names_and_options = { displayed_file_information: file_names, options: OPTIONS }
+  display_names_list = FileDisplay.new(names_and_options)
+  display_names_list.display_file_names
+end
