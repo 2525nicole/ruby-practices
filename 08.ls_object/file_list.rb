8e83959e5bec226ff5edf4e4841b5a32850efa6f
nicole2525 <nicole2525nomail@gmail.com>
2023-07-08 17:33:04 +0900

    FileListクラスの責務を複数ファイルの処理に変更

--- 08.ls_object/file_list.rb
+++ 08.ls_object/file_list.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class FileList
-  attr_reader :file_names_list
+  attr_reader :file_names_list, :file_details_list
 
   def initialize(options)
     @options = options
     @file_names_list = build_file_names_list
+    @file_details_list = build_file_details_list
   end
 
   private
@@ -13,5 +14,15 @@ class FileList
   def build_file_names_list
     file_names = Dir.glob('*', @options['a'] ? File::FNM_DOTMATCH : 0).sort
     @options['r'] ? file_names.reverse : file_names
+  end
+
+  def build_file_details_list
+    file_details_array = []
+    @file_names_list.each do |file_name|
+      args = { file_name:, file_stat: File.lstat(file_name) }
+      file_detail_hash = FileDetail.new(args).file_detail
+      file_details_array << file_detail_hash
+    end
+    file_details_array
   end
 end
