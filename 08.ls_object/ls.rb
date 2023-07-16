e9d60fc6b4301210911342bd48bec4fd6ac5a659
nicole2525 <nicole2525nomail@gmail.com>
2023-07-16 23:32:50 +0900

    ls.rb内のreturn ifの判定条件をシンプルにした

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -8,7 +8,7 @@ require_relative './displayed_file_formatter'
 options = ARGV.getopts('a', 'r', 'l')
 file_list = FileList.new(a_option: options['a'], r_option: options['r'])
 
-return if file_list.file_details.all? { |file| file.file_name.empty? }
+return if file_list.file_details.empty?
 
 displayed_files = DisplayedFileFormatter.new(file_list:, l_option: options['l'])
 displayed_files.display_formatted_files
